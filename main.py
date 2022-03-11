7c4c9a3ddf1d139165b731a96d8a0bcf3a563afa
Juan Josue Zuleta Beb <2971250870108@ingenieria.usac.edu.gt>
2022-03-11 12:28:21 -0600

    aniadiendo metodo de generacion de reportes

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from tkinter.ttk import Combobox
 from tkinter.filedialog import askopenfilename
 from typing import List, Tuple
 
+from jinja2 import Environment, select_autoescape
+from jinja2.loaders import FileSystemLoader
+from os import startfile
+
 class Token:
     def __init__(self, token: str, lexeme: str, row: int, col: int) -> None:
         self.token: str = token
@@ -12,10 +16,7 @@ class Token:
         self.col: int = col
     
     def show_token(self):
-        print(self.token)
-        print(self.lexeme)
-        print(self.row)
-        print(self.col)
+        print('Se ha encontrado el token {} que contiene el lexema {} en la fila {} y columna {}'.format(self.token,self.lexeme,self.row, self.col))
 
 class Errors:
     def __init__(self, line: int, col: int, char: str) -> None:
@@ -24,59 +25,67 @@ class Errors:
         self.char: str = char
     
     def show_errors(self):
-        print(self.line)
-        print(self.char)
+        print('Se ha encontrado el error {} en la fila {} y columna {} '.format(self.char, self.line, self.col))
 
 def automata(starter: str):
-    starter += '\n'
-    tokens: List[Token] = []
-    errores: List[Errors] = []
-    state: int = 0
-    lexeme: str = ''
-    pointer: int = 0
 
+    #agregando al final
+    starter += '\n'
+    #lista de tokens
+    tokens: List[Token] = []
+    #lista de errores
+    errores: List[Errors] = []
+    #estado inicial
+    state: int = 0
+    #estado actual
+    lexeme: str = ''
+    #apuntador
+    pointer: int = 0
+    #Contador de filas y columnas
     row: int = 1
     col: int = 0
+
     while pointer < len(starter):
         char = starter[pointer]
-
         # state inicial
         if state == 0:
             #Lista de transiciones
+            #Si el caracter es un simbolo [~>>[]<>,:]
             if(ord(char) == 60 or ord(char) == 62 or ord(char) == 58 or ord(char) == 44 or ord(char) == 91 or ord(char) == 93 or ord(char) == 126):
                 state = 1
                 pointer += 1
                 col += 1
                 lexeme += char
-
+            #Si el caracter es una letra minuscula [a-z,ñ]
             elif((ord(char) >= 97 and ord(char) <= 122) or ord(char) == 164):
                 state = 2
                 pointer += 1
                 col += 1
                 lexeme += char
-
+            #Si el caracter es una comilla doble ["]
             elif char == '"':
                 state = 3
                 pointer += 1
                 col += 1
                 lexeme += char
-
+            #Si el caracter es una comilla simple [']
             elif char == "'":
                 state = 4
                 pointer += 1
                 col += 1
                 lexeme += char
 
-            # chares ignorados
+            # caracteres ignorados
+            #si es un salto de linea [\n]
             elif (ord(char) == 10):
                 row += 1
                 col = 0
                 pointer += 1
-
+            #si es un tabulador horizontal [\t]
             elif (ord(char) == 9):
                 col += 1
                 pointer += 1
-            
+            #si es un espacio en blanco ['']
             elif (ord(char) == 32):
                 row += 1
                 col = 0
@@ -174,6 +183,26 @@ def automata(starter: str):
     return tuple(tokens), tuple(errores)
 
 
+def process_tokens(tokens):
+    env = Environment(loader=FileSystemLoader('Templates/'),
+                    autoescape=select_autoescape(['html']))
+    template = env.get_template('report_tokens.html')
+
+    html_file = open('oficial_report_tokens.html', 'w+', encoding='utf-8')
+    html_file.write(template.render(tokens=tokens))
+    html_file.close()
+    startfile('oficial_report_tokens.html')
+
+def process_errors(errs):
+    env = Environment(loader=FileSystemLoader('Templates/'),
+                    autoescape=select_autoescape(['html']))
+    template = env.get_template('report_errors.html')
+
+    html_file = open('oficial_report_errors.html', 'w+', encoding='utf-8')
+    html_file.write(template.render(errs=errs))
+    html_file.close()
+    startfile('oficial_report_errors.html')
+
 class display_gui():
     def __init__(self) -> None:
         self.root = tk.Tk()
@@ -200,6 +229,9 @@ class display_gui():
         self.btn_process = Button(self.frame,text="Analizar", command=self.analyzer)
         self.btn_process.place(x=50, y=350)
 
+        self.btn_clear = Button(self.frame,text="Limpiar", command=self.clearText_area)
+        self.btn_clear.place(x=590, y=350)
+
         self.root.resizable(0,0)
         self.root.mainloop()
     
@@ -225,11 +257,20 @@ class display_gui():
     def analyzer(self):
 
         self.getText_area()
-
         tokens, errs = automata(self.text)
-
-        self.clearText_area()          
-
-
+        #self.clearText_area()
+        for i in tokens:
+            i.show_token()
+        print('<>=<>=<>=<>=<>=<>=<>=<>=<>=<> ERRORES <>=<>=<>=<>=<>=<>=<>=<>=<>=<>')
+        for j in errs:
+            j.show_errors()
+        if len(self.text) != 0:
+            if self.combo_report.get() == 'Reporte de Tokens':
+                process_tokens(tokens)
+            elif self.combo_report.get() == 'Reporte de Errores':
+                process_errors(errs)
+        else:
+            print('No hay nada que reportar')
+                
 if __name__ == '__main__':
     display_gui()
